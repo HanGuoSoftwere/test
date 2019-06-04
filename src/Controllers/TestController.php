7feb01778600035ba5006851a7795c87539b974f
chenhao <3537902994>
2019-06-04 19:51:07 +0800

    测试

---
+++ src/Controllers/TestController.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: AltiraPendragon
+ * Date: 2019/6/4
+ * Time: 17:00
+ */
+namespace HanGuoSoft\Test\Controllers;
+
+class TestController extends Controller
+{
+    /**
+     * 测试
+     */
+    public function Test(){
+        return '成功';
+    }
+}
